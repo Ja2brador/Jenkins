81e9bad2427813abbb516037d237c4a054bb5777
bazpiazu002 <azpi13@gmail.com>
2015-12-15 14:02:50 +0100

    Equipo ALLIED
    3 Soldados 4 Médicos

---
+++ jgomas_launcher_pruebaAtaque.bat
@@ -0,0 +1,12 @@
+set rutaAxis="srcJenkins"
+set rutaAllied="src2"
+set tipoAxis=%rutaAxis%"/jasonAgent_AXIS.asl"
+set tipoAxis2=%rutaAxis%"/jasonAgent_AXIS_MEDIC.asl"
+set tipoAxis3=%rutaAxis%"/jasonAgent_AXIS_FIELDOPS.asl"
+set tipoAllied=%rutaAllied%"/jasonAgent_ALLIED_MEDIC.asl"
+set tipoAllied2=%rutaAllied%"/jasonAgent_ALLIED.asl"
+set tipoAllied3=%rutaAllied%"/jasonAgent_ALLIED2.asl"
+set tipoAllied4=%rutaAllied%"/jasonAgent_ALLIED3.asl"
+set fichero="RocososVsJGOMAS.txt"
+set rocososAllied="Corralo:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);Lechuga:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);Adrian:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);Edu:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);Elastico:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied2%);LaCosa:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied3%);Fuego:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied4%);"
+java -classpath "lib\jade.jar;lib\jadeTools.jar;lib\Base64.jar;lib\http.jar;lib\iiop.jar;lib\beangenerator.jar;lib\jgomas.jar;lib\jason.jar;lib\JasonJGomas.jar;classes;." jade.Boot -container -host localhost "T1:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);T2:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);T3:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);T4:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);T5:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);T6:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);T7:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);"%rocososAllied%>%fichero%
